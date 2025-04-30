e57879f49776220a556f06400378f364a4c8f1b3
Hanna Syniavska <annarybalko34@gmail.com>
2025-04-30 17:31:54 +0200

    added tests names of iterations

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -35,8 +35,19 @@ it("should show the total price of weighted product", async () => {
   expect(total).toHaveValue("3.38");
 });
 
+// Second iteration with technical slices
 it("should add the last weighted product in the sidebar", async () => {});
 
 it("should show the list of weighted products with total price", async () => {});
 
-it("should show the list of weighted products with product names", async () => {});
+it("should show the list of weighted products with product names and total price", async () => {});
+
+// Third iteration of errors more "freedom"
+it("should show the the error when the product is not chosen", async () => {});
+
+it("should show the error when the product is not weighted", async () => {});
+
+// Fourth iteration of cleaning
+it("should clear inputs", async () => {});
+
+it("should clear list", async () => {});
