e6ceaaf62f06384195f99583c97e90f8371141a9
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-05-09 12:28:42 +0200

    fix

---
+++ src/.prettierrc.js
@@ -0,0 +1,23 @@
+const { readdirSync } = require('fs')
+const { baseUrl } = require('./tsconfig.json').compilerOptions
+
+const path = `${__dirname}/${baseUrl}`
+const options = { withFileTypes: true }
+const baseUrlSubdirectories = readdirSync(path, options)
+  .filter((entry) => entry.isDirectory())
+  .map((entry) => entry.name)
+const baseUrlSubdirectoryRegex = `^(${baseUrlSubdirectories.join('|')})`
+
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^@mercadona/(.*)$',
+    baseUrlSubdirectoryRegex,
+    '^[./]'
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
